fbb457041203e0d64e588512d40071309bcc84fe
Luiz Oliveira <luizsoliveira@gmail.com>
2024-09-18 22:01:35 -0300

    Publishing collections

--- server/main.js
+++ server/main.js
@@ -1,9 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 import { loadInitialData } from '../infra/initial-data';
 
+import { Communities } from '../communities/communities';
+import { People } from '../people/people';
+import '../people/methods';
+
 Meteor.startup(async () => {
   // DON'T CHANGE THE NEXT LINE
   await loadInitialData();
 
   // YOU CAN DO WHATEVER YOU WANT HERE
 });
+
+// Publishing collections
+Meteor.publish('communities', () => Communities.find());
+
+Meteor.publish('people', () => People.find());
